bec1ab8cb4c15fe7b2af1160f821447bde9c762a
knobli <raffael@santschi.ch>
2014-06-10 18:45:57 +0200

    add while loop

--- actor_in_c.c
+++ actor_in_c.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <mqueue.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -11,75 +13,82 @@
 #define QUEUE_NAME "/messages"
 
 struct actor_arg {
-    char *msg;
-    ssize_t length;
+	char *msg;
+	ssize_t length;
 };
 
 /* the wanna-be actor. is run by
  * a thread per incoming message */
 void *run_actor(void *arg) {
-    struct actor_arg *params = arg;
-    printf("Actor received Message(%zu): [%s]\n",
-            params->length, params->msg);
-    free(params->msg);
+	struct actor_arg *params = arg;
+	printf("Actor received Message(%zu): [%s]\n", params->length, params->msg);
+	free(params->msg);
 
-    pthread_exit(NULL);
+	pthread_exit(NULL);
 }
 
 /* single thread constantly waiting
  * for new messages in the queue.
  * creates actors per message */
 void *dispatch(void *arg) {
-    // open queue
-    mqd_t mq = mq_open(QUEUE_NAME, O_RDONLY);
-    assert(mq != -1);
+	// open queue
+	mqd_t mq = mq_open(QUEUE_NAME, O_RDONLY | O_NONBLOCK);
+	assert(mq != -1);
 
-    int ret;
-    ssize_t nbytes;
-    char *buf;
-    pthread_t *actor;
-    struct actor_arg *aarg;
+	int ret;
+	ssize_t nbytes;
+	char *buf;
+	pthread_t *actor;
+	struct actor_arg *aarg;
 
-    // todo: make the following a loop
+	// todo: make the following a loop
 
-    // wait for and dispatch messages
-    buf = malloc(sizeof(char) * 1024);
-    assert(buf != NULL);
-    nbytes = mq_receive(mq, buf, 1024, NULL);
-    actor = malloc(sizeof(pthread_t));
-    aarg = malloc(sizeof(struct actor_arg));
-    aarg->msg = buf;
-    aarg->length = nbytes;
+	// wait for and dispatch messages
+	buf = malloc(sizeof(char) * 1024);
+	assert(buf != NULL);
+	while ((nbytes = mq_receive(mq, buf, 1024, 0)) > 0) {
+		printf("Test\n");
+		actor = malloc(sizeof(pthread_t));
+		aarg = malloc(sizeof(struct actor_arg));
+		aarg->msg = buf;
+		aarg->length = nbytes;
 
-
-    ret = pthread_create(actor, NULL, run_actor, aarg);
-    assert(ret == 0);
-
-    pthread_join(*actor, NULL);
-    pthread_exit(NULL);
+		ret = pthread_create(actor, NULL, run_actor, aarg);
+		assert(ret == 0);
+		printf("Test3\n");
+		pthread_join(*actor, NULL);
+		printf("Test4\n");
+	}
+	printf("Test\n");
+	pthread_exit(NULL);
 }
 
 int main(int argc, char** argv) {
-    int ret;
+	int ret;
 
-    // create queue, open for writing
-    mqd_t mq = mq_open(QUEUE_NAME, O_CREAT | O_WRONLY, 0644, 0);
-    assert(mq != -1);
+	struct mq_attr mqAttr;
+    mqAttr.mq_maxmsg = 10;
+    mqAttr.mq_msgsize = 1024;
 
-    // spawn the dispatcher
-    pthread_t dispatcher;
-    ret = pthread_create(&dispatcher, NULL, dispatch, NULL);
-    assert(ret == 0);
+	// create queue, open for writing
+	mqd_t mq = mq_open(QUEUE_NAME, O_CREAT | O_WRONLY, 0644, &mqAttr);
+	assert(mq != -1);
 
-    // send a message
-    char *msg = strdup("hello");
-    ret = mq_send(mq, msg, strlen(msg)+1, 0);
-    assert(ret == 0);
+	// send a message
+	char *msg = strdup("hello");
+	ret = mq_send(mq, msg, strlen(msg) + 1, 0);
+	assert(ret == 0);
 
-    // wait for dispatcher to complete
-    pthread_join(dispatcher, NULL);
+	// spawn the dispatcher
+	pthread_t dispatcher;
+	ret = pthread_create(&dispatcher, NULL, dispatch, NULL);
+	assert(ret == 0);
 
-    mq_unlink(QUEUE_NAME);
+	printf("Test2\n");
+	// wait for dispatcher to complete
+	pthread_join(dispatcher, NULL);
 
-    return EXIT_SUCCESS;
+	mq_unlink(QUEUE_NAME);
+
+	return EXIT_SUCCESS;
 }
